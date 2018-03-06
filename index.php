5681781bf6067ab22a80e7278af1375930ff2546
mzwae <mrmonjaie@gmail.com>
2018-03-06 17:40:09 +0000

    added Controller class

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 require('config.php');
 require('classes/Bootstrap.php');
+require('classes/Controller.php');
 
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
+
+if ($controller) {
+    $controller->executeAction();
+}
