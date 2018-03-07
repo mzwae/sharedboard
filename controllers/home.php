a4402087a3a8bf453a9497d3f7ff9657cb2efc33
mzwae <mrmonjaie@gmail.com>
2018-03-07 23:21:02 +0000

    added Ctrls and models

--- controllers/home.php
+++ controllers/home.php
@@ -4,6 +4,7 @@ class Home extends Controller
 {
     protected function Index()
     {
-        echo 'HOME/INDEX';
+        $viewmodel = new HomeModel();
+        $this->ReturnView($viewmodel->Index(), true);
     }
 }
--- controllers/shares.php
+++ controllers/shares.php
@@ -4,6 +4,7 @@ class Shares extends Controller
 {
     protected function Index()
     {
-        echo 'SHARES/INDEX';
+        $viewmodel = new ShareModel();
+        $this->ReturnView($viewmodel->Index(), true);
     }
 }
