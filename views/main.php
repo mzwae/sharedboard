f731270ad4a71060ba85a5ed7ea033d2b25e3fa6
mzwae <mrmonjaie@gmail.com>
2018-03-07 23:34:29 +0000

    added bootstrap css

---
+++ views/main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sharedboard</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <?php require($view); ?>
+  </body>
+</html>
