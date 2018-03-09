6fef06103600f231a10429b4c92b8e953224f5bf
mzwae <mrmonjaie@gmail.com>
2018-03-09 22:07:33 +0000

    added user registration

--- models/user.php
+++ models/user.php
@@ -2,8 +2,29 @@
 
 class UserModel extends Model
 {
-    public function index()
+    public function register()
     {
-        return;
+      // Sanitize POST
+      $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+      $password = md5($post['password']);
+
+      if ($post['submit']) {
+        // die("Submitted");
+        // Insert into MySQL
+        $this->query('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+        $this->bind(':name', $post['name']);
+        $this->bind(':email', $post['email']);
+        $this->bind(':password', $post['password']);
+        $this->execute();
+
+        // Verify
+        if ($this->lastInsertId()) {
+          // Redirect
+          header('Location: ' . ROOT_URL . 'users/login');
+        }
+      }
+
+      return;
     }
 }
